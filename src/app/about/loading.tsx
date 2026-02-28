243b1ae0bca5bf1a7bc109180449ea211eb87573
killuaZolyck69 <tanver694225@gmail.com>
2026-02-28 15:56:07 +0600

    feat: add theme support and about page UI states
    Integrate next-themes for dark mode support. Add a ThemeProvider to the
    root layout and a ModeToggle component to the navbar.
    
    Include new UI components for DropdownMenu and Spinner. Update the
    about page with error and loading boundaries, including an intentional
    delay and error throw for testing purposes.

---
+++ src/app/about/loading.tsx
@@ -0,0 +1,6 @@
+import { Spinner } from "@/components/ui/spinner";
+
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return <Spinner></Spinner>;
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,10 @@
-export default function AboutPage() {
+export default async function AboutPage() {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+
+  throw new Error("Somethng went wrong");
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        This is About Page
+      This is About Page
     </div>
   );
 }

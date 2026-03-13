0a43a3aba287b93ad88bbf4c6791b2c49c609339
killuaZolyck69 <tanver694225@gmail.com>
2026-03-14 01:07:53 +0600

    feat: integrate better-auth for authentication
    Install better-auth dependency and configure the auth client.
    Implement Google social login in the LoginForm component and update
     the home page to log the current session for verification.

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
+  const session = authClient.getSession();
+
+  console.log(session);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Button>Click me</Button>

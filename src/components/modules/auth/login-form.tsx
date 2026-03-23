575412a52b4ca43f0c6c32c834ebc30b36cb7bf2
killuaZolyck69 <tanver694225@gmail.com>
2026-03-23 16:10:09 +0600

    feat(auth): implement form handling and toasts
    Integrate @tanstack/react-form and Zod for validation in login and
    registration forms. Add sonner for toast notifications to provide
    feedback during authentication.
    
    Update ESLint rules to allow function children, supporting TanStack
    Form's render prop pattern. Add a custom Toaster component and
    configure it in the root layout.

--- src/components/modules/auth/login-form.tsx
+++ src/components/modules/auth/login-form.tsx
@@ -6,17 +6,27 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
+import { useForm } from "@tanstack/react-form";
+import * as z from "zod";
+import { toast } from "sonner";
+
+const formSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8, "Minimum length is 8"),
+});
 
 export function LoginForm({
   className,
@@ -33,6 +43,33 @@ export function LoginForm({
 
   console.log(session);
 
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    validators: {
+      onSubmit: formSchema,
+    },
+    onSubmit: async ({ value }) => {
+      const toastId = toast.loading("User Login");
+
+      try {
+        const { data, error } = await authClient.signIn.email(value);
+
+        if (error) {
+          toast.error(error.message, { id: toastId });
+          return;
+        }
+
+        toast.success("User Login Successfully", { id: toastId });
+        console.log(data);
+      } catch (error) {
+        toast.error("Something went wrong, please try again.", { id: toastId });
+      }
+    },
+  });
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -43,44 +80,76 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form
+            id="login-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              form.handleSubmit();
+            }}
+          >
             <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </Field>
-              <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </Field>
-              <Field>
-                <Button type="submit">Login</Button>
-                <Button
-                  onClick={() => handleGoogleLogIn()}
-                  variant="outline"
-                  type="button"
-                >
-                  Login with Google
-                </Button>
-                <FieldDescription className="text-center">
-                  Don&apos;t have an account? <a href="#">Sign up</a>
-                </FieldDescription>
-              </Field>
+              <form.Field
+                name="email"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                      <Input
+                        type="email"
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+              <form.Field
+                name="password"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                      <Input
+                        type="password"
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
             </FieldGroup>
           </form>
+          <CardFooter className="flex flex-col gap-6 mt-5">
+            <Button form="login-form" className="w-full">
+              Login
+            </Button>
+            <Button
+              onClick={() => handleGoogleLogIn()}
+              variant="outline"
+              type="button"
+              className="w-full"
+            >
+              Login with Google
+            </Button>
+            <FieldDescription className="text-center">
+              Don&apos;t have an account? <a href="/register">Sign up</a>
+            </FieldDescription>
+          </CardFooter>
         </CardContent>
       </Card>
     </div>
